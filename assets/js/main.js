cf675b9a83794422afd2a1721a47dfb381c5ce08
Gordo Mula <brandonlouisferreira@gmail.com>
2022-10-23 23:29:34 -0300

    responsive clothing

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,19 @@ const scrollHeader = () =>{
 window;addEventListener('scroll', scrollHeader)
 
 /*=============== SWIPER PRODUCTS ===============*/
+let swiperProducts = new Swiper(".products__container", {
+    spaceBetween: 32,
+    grabCursor: true, 
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
 
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
