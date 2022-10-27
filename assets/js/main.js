6959cb814cbae99d1de2f0a64e596e7ac83c6c47
Brandon louis <brandonlouisferreira@gmail.com>
2022-10-27 18:28:07 -0300

    Responsive-clothing

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,6 +57,46 @@ const scrollUp = () =>{
 window.addEventListener('scroll', scrollUp)
 
 /*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
 
+//previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+//We obtain the current theme that the interface has by validating the dark-theme class 
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+//We validate if the user previously choose a topic 
+if (selectedTheme) {
+    //If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark 
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+//activate /deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    //add or remove the dark /icon theme    
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    //We save the theme and current icon that the user chose 
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    //reset: true
+})
+
+sr.reveal(`.home__data, .products__container, .footer__container, .footer__info`)
+sr.reveal(`.home__images`,{delay: 600, origin: 'bottom'})
+sr.reveal(`.new__card, .brand__img`,{interval: 100})
+sr.reveal(`.collection__explore:nth-child(1)`,{origin: 'right'})
+sr.reveal(`.collection__explore:nth-child(2)`,{origin: 'left'})
